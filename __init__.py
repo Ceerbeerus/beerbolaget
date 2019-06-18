e54d648cab6a2e79f25ca7abd9d8bbe70f300850
git <kallin.robert@gmail.com>
2019-06-18 23:58:04 +0200

    Updated beerbolaget python package version.

--- __init__.py
+++ __init__.py
@@ -1,19 +1,20 @@
 """
-A component which allows you to view the latest batch of beer at the swedish Systembolaget.
-For more details about this component, please refer to the documentation at
+A component which allows you to view the latest
+batch of beer at the swedish Systembolaget.
+For more details about this component,
+please refer to the documentation at
 https://github.com/Ceerbeerus/systembolaget
 """
 import logging
-import os.path
 
-import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
 
 VERSION = '0.0.1'
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['pybeerbolaget==0.0.8']
+REQUIREMENTS = ['beerbolaget==0.0.36b0']
 
 CONF_API_KEY = 'api_key'
 CONF_RATEBEER = 'ratebeer'
@@ -23,12 +24,12 @@ DOMAIN = 'beerbolaget'
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
-        vol.Optional(CONF_API_KEY, default=''):
-            vol.All(cv.ensure_list, [cv.string]),
-        vol.Optional(CONF_RATEBEER, default=False): cv.boolean,
-        vol.Optional(CONF_UNTAPPD, default=False): cv.boolean
+        vol.Optional(CONF_API_KEY, default=''): cv.string,
+        vol.Optional(CONF_RATEBEER, default=''): cv.string,
+        vol.Optional(CONF_UNTAPPD, default=''): cv.string
     })
 }, extra=vol.ALLOW_EXTRA)
+
 
 async def async_setup(hass, config):
     """Set up this component"""
@@ -36,26 +37,32 @@ async def async_setup(hass, config):
     conf_ratebeer = config[DOMAIN][CONF_RATEBEER]
     conf_untappd = config[DOMAIN][CONF_UNTAPPD]
 
-    _LOGGER.info('if you have any issues with this component, please report them here:'
-                 ' https://github.com/Ceerbeerus/systembolaget')
-    _LOGGER.debug('Version %s', VERSION)
+    if not conf_api_key:
+        _LOGGER.error("API_KEY is required to use this component.")
+        return False
+
+    _LOGGER.info("if you have any issues with this component,"
+                 " please report them here:"
+                 " https://github.com/Ceerbeerus/beerbolaget")
+    _LOGGER.debug("Version %s", VERSION)
 
     latest_release = release(hass, conf_api_key, conf_ratebeer, conf_untappd)
 
     async def check_release_service(call):
         await latest_release.check_release()
 
-    hass.services.async_register(DOMAIN, 'check_release', check_release_service)
+    hass.services.async_register(DOMAIN, 'check_release',
+                                 check_release_service)
     return True
 
 
 class release():
     def __init__(self, hass, conf_api_key, conf_ratebeer, conf_untappd):
-        _LOGGER.debug('Systembolaget - __init__')
-        from pybeerbolaget.ha_custom.beer import beer_handler
+        _LOGGER.debug("Systembolaget - __init__")
+        from beerbolaget.ha_custom.beer import beer_handler
         self.hass = hass
         self.api_key = conf_api_key
-        self.beer = beer_handler(self.api_key)
+        self.beer = beer_handler(self.api_key, conf_ratebeer, conf_untappd)
         self.ratebeer = conf_ratebeer
         self.untappd = conf_untappd
         self.release = None
@@ -68,4 +75,5 @@ class release():
         attributes = {
             'beer': beers
         }
-        self.hass.states.async_set('sensor.systembolaget_new_release', self.release, attributes)
+        self.hass.states.async_set('sensor.beerbolaget_new_release',
+                                   self.release, attributes)
