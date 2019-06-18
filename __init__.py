23659f1c9f78ccd05c6c150f1da17995741e9f33
Ceerbeerus <46457478+Ceerbeerus@users.noreply.github.com>
2019-06-18 10:50:39 +0000

    Updated component name and removed wine and whisky parts.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 """
-A component which allows you to view the latest batch of goods at the swedish Systembolaget.
+A component which allows you to view the latest batch of beer at the swedish Systembolaget.
 For more details about this component, please refer to the documentation at
 https://github.com/Ceerbeerus/systembolaget
 """
@@ -16,34 +16,31 @@ _LOGGER = logging.getLogger(__name__)
 REQUIREMENTS = ['pybeerbolaget==0.0.8']
 
 CONF_API_KEY = 'api_key'
-CONF_SHOW_BEER = 'show_beer'
-CONF_SHOW_WINE = 'show_wine'
-CONF_SHOW_WHISKY = 'show_whisky'
+CONF_RATEBEER = 'ratebeer'
+CONF_UNTAPPD = 'untappd'
 
-DOMAIN = 'systembolaget'
+DOMAIN = 'beerbolaget'
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Optional(CONF_API_KEY, default=''):
             vol.All(cv.ensure_list, [cv.string]),
-        vol.Optional(CONF_SHOW_BEER, default=False): cv.boolean,
-        vol.Optional(CONF_SHOW_WINE, default=False): cv.boolean,
-        vol.Optional(CONF_SHOW_WHISKY, default=False): cv.boolean
+        vol.Optional(CONF_RATEBEER, default=False): cv.boolean,
+        vol.Optional(CONF_UNTAPPD, default=False): cv.boolean
     })
 }, extra=vol.ALLOW_EXTRA)
 
 async def async_setup(hass, config):
     """Set up this component"""
     conf_api_key = config[DOMAIN][CONF_API_KEY]
-    conf_beer = config[DOMAIN][CONF_SHOW_BEER]
-    conf_wine = config[DOMAIN][CONF_SHOW_WINE]
-    conf_whisky = config[DOMAIN][CONF_SHOW_WINE]
+    conf_ratebeer = config[DOMAIN][CONF_RATEBEER]
+    conf_untappd = config[DOMAIN][CONF_UNTAPPD]
 
     _LOGGER.info('if you have any issues with this component, please report them here:'
                  ' https://github.com/Ceerbeerus/systembolaget')
     _LOGGER.debug('Version %s', VERSION)
 
-    latest_release = release(hass, conf_api_key, conf_beer, conf_wine, conf_whisky)
+    latest_release = release(hass, conf_api_key, conf_ratebeer, conf_untappd)
 
     async def check_release_service(call):
         await latest_release.check_release()
@@ -53,14 +50,14 @@ async def async_setup(hass, config):
 
 
 class release():
-    def __init__(self, hass, conf_api_key, conf_beer, conf_wine, conf_whisky):
+    def __init__(self, hass, conf_api_key, conf_ratebeer, conf_untappd):
         _LOGGER.debug('Systembolaget - __init__')
         from pybeerbolaget.ha_custom.beer import beer_handler
         self.hass = hass
         self.api_key = conf_api_key
-        self.beer = beer_handler(self.api_key, True)
-        self.wine = conf_wine
-        self.whisky = conf_whisky
+        self.beer = beer_handler(self.api_key)
+        self.ratebeer = conf_ratebeer
+        self.untappd = conf_untappd
         self.release = None
 
     async def check_release(self):
@@ -69,8 +66,6 @@ class release():
         self.release = await self.beer.get_release()
         beers = await self.beer.get_beers()
         attributes = {
-            'beer': beers,
-            'wine': self.wine,
-            'whisky': self.whisky
+            'beer': beers
         }
         self.hass.states.async_set('sensor.systembolaget_new_release', self.release, attributes)
