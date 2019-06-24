fd542b4a914b848155b1c95524fdb8c5bf702019
git <kallin.robert@gmail.com>
2019-06-24 22:52:14 +0200

    Added information about availability at local store and updated beerbolaget version to 0.0.36b4.

--- __init__.py
+++ __init__.py
@@ -6,26 +6,33 @@ please refer to the documentation at
 https://github.com/Ceerbeerus/beerbolaget
 """
 import logging
+from datetime import timedelta
 
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
+from homeassistant.const import EVENT_HOMEASSISTANT_START
+from homeassistant.helpers.event import async_track_time_interval
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['beerbolaget==0.0.36b3']
+REQUIREMENTS = ['beerbolaget==0.0.36b4']
 
 CONF_API_KEY = 'api_key'
 CONF_RATEBEER = 'ratebeer'
+CONF_STORE = 'store'
 CONF_UNTAPPD = 'untappd'
 
 DOMAIN = 'beerbolaget'
+
+INTERVAL = timedelta(days=1)
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Optional(CONF_API_KEY, default=''): cv.string,
         vol.Optional(CONF_RATEBEER, default=''): cv.string,
+        vol.Optional(CONF_STORE, default=''): cv.string,
         vol.Optional(CONF_UNTAPPD, default=''): cv.string
     })
 }, extra=vol.ALLOW_EXTRA)
@@ -35,6 +42,7 @@ async def async_setup(hass, config):
     """Set up this component"""
     conf_api_key = config[DOMAIN][CONF_API_KEY]
     conf_ratebeer = config[DOMAIN][CONF_RATEBEER]
+    conf_store = config[DOMAIN][CONF_STORE]
     conf_untappd = config[DOMAIN][CONF_UNTAPPD]
 
     if not conf_api_key:
@@ -46,7 +54,13 @@ async def async_setup(hass, config):
                  " https://github.com/Ceerbeerus/beerbolaget")
     _LOGGER.debug("Version %s", VERSION)
 
-    latest_release = release(hass, conf_api_key, conf_ratebeer, conf_untappd)
+    latest_release = release(hass, conf_api_key, conf_ratebeer,
+                             conf_store, conf_untappd)
+
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, latest_release.check_release())
+
+    async_track_time_interval(
+                hass, latest_release.check_release, INTERVAL)
 
     async def check_release_service(call):
         await latest_release.check_release()
@@ -57,21 +71,25 @@ async def async_setup(hass, config):
 
 
 class release():
-    def __init__(self, hass, conf_api_key, conf_ratebeer, conf_untappd):
+    def __init__(self, hass, conf_api_key,
+                 conf_ratebeer, conf_store, conf_untappd):
         _LOGGER.debug("Systembolaget - __init__")
         from beerbolaget.ha_custom.beer import beer_handler
         self.hass = hass
         self.api_key = conf_api_key
-        self.beer = beer_handler(self.api_key, conf_ratebeer, conf_untappd)
+        self.beer_handler = beer_handler(self.api_key, conf_ratebeer,
+                                         conf_store, conf_untappd)
         self.ratebeer = conf_ratebeer
+        self.store = conf_store
         self.untappd = conf_untappd
         self.release = None
 
     async def check_release(self):
         beers = []
-        await self.beer.update_new_beers()
-        self.release = await self.beer.get_release()
-        beers = await self.beer.get_beers()
+        await self.beer_handler.get_store_info()
+        await self.beer_handler.update_new_beers()
+        self.release = await self.beer_handler.get_release()
+        beers = await self.beer_handler.get_beers()
         attributes = {
             'beer': beers
         }
