f6e262cee6883cc1cecc1a75c325594593ca7731
git <kallin.robert@gmail.com>
2019-06-26 21:09:17 +0200

    Added image_url as option and bumped beerbolaget lib to 0.0.36b6.

--- beerbolaget/__init__.py
+++ beerbolaget/__init__.py
@@ -17,9 +17,10 @@ VERSION = '0.0.2'
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['beerbolaget==0.0.36b4']
+REQUIREMENTS = ['beerbolaget==0.0.36b6']
 
 CONF_API_KEY = 'api_key'
+CONF_IMAGE_URL = 'image_url'
 CONF_RATEBEER = 'ratebeer'
 CONF_STORE = 'store'
 CONF_UNTAPPD = 'untappd'
@@ -31,6 +32,7 @@ INTERVAL = timedelta(days=1)
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Optional(CONF_API_KEY, default=''): cv.string,
+        vol.Optional(CONF_IMAGE_URL, default=''): cv.string,
         vol.Optional(CONF_RATEBEER, default=''): cv.string,
         vol.Optional(CONF_STORE, default=''): cv.string,
         vol.Optional(CONF_UNTAPPD, default=''): cv.string
@@ -41,6 +43,7 @@ CONFIG_SCHEMA = vol.Schema({
 async def async_setup(hass, config):
     """Set up this component"""
     conf_api_key = config[DOMAIN][CONF_API_KEY]
+    conf_image_url = config[DOMAIN][CONF_IMAGE_URL]
     conf_ratebeer = config[DOMAIN][CONF_RATEBEER]
     conf_store = config[DOMAIN][CONF_STORE]
     conf_untappd = config[DOMAIN][CONF_UNTAPPD]
@@ -54,10 +57,11 @@ async def async_setup(hass, config):
                  " https://github.com/Ceerbeerus/beerbolaget")
     _LOGGER.debug("Version %s", VERSION)
 
-    latest_release = release(hass, conf_api_key, conf_ratebeer,
-                             conf_store, conf_untappd)
+    latest_release = release(hass, conf_api_key, conf_image_url,
+                             conf_ratebeer, conf_store, conf_untappd)
 
-    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, latest_release.check_release())
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START,
+                               latest_release.check_release())
 
     async_track_time_interval(
                 hass, latest_release.check_release, INTERVAL)
@@ -71,23 +75,24 @@ async def async_setup(hass, config):
 
 
 class release():
-    def __init__(self, hass, conf_api_key,
+    def __init__(self, hass, conf_api_key, conf_image_url,
                  conf_ratebeer, conf_store, conf_untappd):
         _LOGGER.debug("Systembolaget - __init__")
         from beerbolaget.ha_custom.beer import beer_handler
         self.hass = hass
         self.api_key = conf_api_key
-        self.beer_handler = beer_handler(self.api_key, conf_ratebeer,
-                                         conf_store, conf_untappd)
-        self.ratebeer = conf_ratebeer
-        self.store = conf_store
-        self.untappd = conf_untappd
+        self.beer_handler = beer_handler(self.api_key,
+                                         conf_image_url,
+                                         conf_ratebeer,
+                                         conf_store,
+                                         conf_untappd)
         self.release = None
 
     async def check_release(self):
         beers = []
         await self.beer_handler.get_store_info()
         await self.beer_handler.update_new_beers()
+        await self.beer_handler.get_images()
         self.release = await self.beer_handler.get_release()
         beers = await self.beer_handler.get_beers()
         attributes = {
