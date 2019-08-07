f9ec40e8f68d6a15b50d5befbd878b74bbbc7ec5
git <kallin.robert@gmail.com>
2019-08-07 10:04:33 +0200

    Bumped library version.

--- custom_components/beerbolaget/__init__.py
+++ custom_components/beerbolaget/__init__.py
@@ -11,7 +11,7 @@ import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 from homeassistant.helpers.discovery import load_platform
 
-__version__ = '0.3.6'
+__version__ = '0.3.7'
 
 _LOGGER = logging.getLogger(__name__)
 
