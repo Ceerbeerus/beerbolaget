aea46d5ed9a58fa7be020f91a3007330ee9f3a68
Ceerbeerus <46457478+Ceerbeerus@users.noreply.github.com>
2019-11-27 00:20:02 +0100

    Update __init__.py
    Bumped library version for error handling.

--- custom_components/beerbolaget/__init__.py
+++ custom_components/beerbolaget/__init__.py
@@ -11,7 +11,7 @@ import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 from homeassistant.helpers.discovery import load_platform
 
-__version__ = '0.3.11'
+__version__ = '0.3.12'
 
 _LOGGER = logging.getLogger(__name__)
 
