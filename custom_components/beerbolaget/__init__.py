6b02c218e18a8e57585e871cc37268fbf8171034
Ceerbeerus <46457478+Ceerbeerus@users.noreply.github.com>
2020-01-10 17:30:10 +0100

    Update __init__.py

--- custom_components/beerbolaget/__init__.py
+++ custom_components/beerbolaget/__init__.py
@@ -11,7 +11,7 @@ import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 from homeassistant.helpers.discovery import load_platform
 
-__version__ = '0.3.12'
+__version__ = '0.3.13'
 
 _LOGGER = logging.getLogger(__name__)
 
