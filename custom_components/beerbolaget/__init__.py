075ea8557f0d2b53784a85cf70e072377f267fa9
Ceerbeerus <46457478+Ceerbeerus@users.noreply.github.com>
2020-05-08 10:57:08 +0200

    Update __init__.py

--- custom_components/beerbolaget/__init__.py
+++ custom_components/beerbolaget/__init__.py
@@ -15,7 +15,7 @@ from homeassistant.core import callback
 
 from homeassistant.helpers.discovery import load_platform
 
-__version__ = '0.4.5'
+__version__ = '0.4.6'
 
 _LOGGER = logging.getLogger(__name__)
 
